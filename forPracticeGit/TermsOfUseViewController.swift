d1994b4f1b4ac1d9de2460a3b0bf0a977aae3fe7
Okamura-Koudai <okamura@www-biz.co>
2022-07-10 14:28:19 +0900

    フォルダ追加

---
+++ forPracticeGit/TermsOfUseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TermsOfUseViewController.swift
+//  LABOOK
+//
+//  Created by Koudai Okamura on 2022/03/11.
+//
+
+import UIKit
+
+class TermsOfUseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
